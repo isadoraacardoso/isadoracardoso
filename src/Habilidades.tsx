777fa2a9736540cafb558b1136ca9ca51603ed6b
Isadora Cardoso <101332900+isadoraacardoso@users.noreply.github.com>
2025-05-25 15:54:07 -0300

    chore: skills

--- src/Habilidades.tsx
+++ src/Habilidades.tsx
@@ -24,8 +24,20 @@ const habilidades = [
     icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
   },
   {
+    name: "Tailwind CSS",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg",
+  },
+  {
     name: "Node.js",
     icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+  },
+  {
+    name: "Next.js",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+  },
+  {
+    name: "Prisma",
+    icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/prisma/prisma-original.svg",
   },
 ];
 
@@ -63,7 +75,7 @@ function Habilidades() {
       <section>
         <div className="title-projects">
           <h2>
-            Habilidades <span className="emoji">🔥</span>
+            Habilidades <span className="emoji"> 🔥</span>
           </h2>
         </div>
         <div className="skills-grid">
